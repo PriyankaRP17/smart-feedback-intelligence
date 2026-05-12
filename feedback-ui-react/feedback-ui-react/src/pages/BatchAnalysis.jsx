0d98e3c63e5c9fdd93e5fdec3022b633c5fdeba2
PriyankaRP17 <priyankapremnath17@gmail.com>
2026-05-12 16:07:41 +0530

    Fix API URL to use env variable

--- feedback-ui-react/feedback-ui-react/src/pages/BatchAnalysis.jsx
+++ feedback-ui-react/feedback-ui-react/src/pages/BatchAnalysis.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import axios from 'axios'
 
-const API = 'http://localhost:8000'
+const API = import.meta.env.VITE_API_URL || 'http://localhost:8000'
 
 const SENTIMENT_COLOR = { negative: 'var(--neg)', positive: 'var(--pos)', neutral: 'var(--neu)' }
 const URGENCY_COLOR   = { high: 'var(--neg)', medium: 'var(--neu)', low: 'var(--pos)' }
